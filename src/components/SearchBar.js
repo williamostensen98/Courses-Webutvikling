f03bcff76f17a34a335fa5d39f44b60e1ec485a4
Victor Jørgensen <victojo@stud.ntnu.no>
2019-10-08 16:36:57 +0200

    Searchbar #1

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/main.css';
+
+export default function SearchBar() {
+    return (
+        <div className="searchbar-container">
+              <input id="searchBar" type="text" placeholder="Search.."/>
+        </div>
+    )
+}
+
+
