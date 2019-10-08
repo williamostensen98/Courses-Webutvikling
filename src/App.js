f52090f1abcdc0aa6067478d6b15f200aa27e959
Jenny Yu <jennyjinyiyu@gmail.com>
2019-10-08 16:56:10 +0200

    made header and easy styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './css/main.css';
 import SearchBar from './components/SearchBar';
 import {createStore} from "redux";
+import Header from "./components/Header"
 
 export default function App() {
   return ( 
     <div className="App"> 
+      <Header />
       <SearchBar />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../css/header.css"
+
+function Header() {
+    return (
+        <div className="header">GRUPPE 39</div>
+    )
+}
+
+export default Header
