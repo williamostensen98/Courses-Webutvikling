8d6a6071159ba6e7f68cac6340467cc131a4e33f
Jenny Yu <jennyjinyiyu@gmail.com>
2019-10-09 11:43:11 +0200

    fixed imports of css files in App.js and removed body-margin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import './css/main.css';
+import "./default_files/App.css"
+import "./default_files/index.css"
 import SearchBar from './components/SearchBar';
 import {createStore} from "redux";
 import Header from "./components/Header"
 
 export default function App() {
   return ( 
-    <div className="App"> 
-      <Header />
-      <SearchBar />
-    </div>
+    <body>
+      <div className="App"> 
+        <Header />
+        <SearchBar />
+      </div>
+    </body>
   );
 }
