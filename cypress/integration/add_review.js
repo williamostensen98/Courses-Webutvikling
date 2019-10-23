65d71716955f206043762f6f4fa1b52607e6fa42
Victor Jørgensen <victojo@stud.ntnu.no>
2019-10-23 13:33:51 +0200

    Test for å sjekke at db oppdaterer seg på submit av review er lagt til. #5

--- cypress/integration/add_review.js
+++ cypress/integration/add_review.js
@@ -1,13 +1,21 @@
 import axios from 'axios'
+var REVIEW_TEXT = "This course is amazing!"
+
+// Refering to the documentation for Cypress, this test sets up an local development server on localhost. 
+// Src: https://docs.cypress.io/guides/getting-started/testing-your-app.html#Step-1-Start-your-server
+
 
 describe('Test to write an invalid review with difficulty out of range', function() {
     it('Searches for a course and tries to add a invalid review and a rating of the difficulty out of range.', function() {
+    //  Executing start of backend and frontend 
+    //   cy.exec('npm start --prefix backend/')
+    //   cy.exec('npm start --prefix frontend/')
       cy.visit('http://localhost:3000')
       cy.get('.mr-sm-2').type('IT2805')
       cy.get('form').contains("Search").click()
       cy.get('.col > .fas').click()
       cy.get('#rating > .mt-4').click()
-      cy.get('#review').type("This course is amazing!")
+      cy.get('#review').type(REVIEW_TEXT)
       cy.get('#difficulty').type('6')
       cy.get(':nth-child(2) > form > .btn').click()
       cy.get('#review').should("have.value", 'This course is amazing!')
@@ -22,13 +30,21 @@ describe('Test to write an invalid review with difficulty out of range', functio
       cy.get('form').contains("Search").click()
       cy.get('.col > .fas').click()
       cy.get('#rating > .mt-4').click()
-      cy.get('#review').type("This course is amazing!")
+      cy.get('#review').type(REVIEW_TEXT)
       cy.get('#difficulty').type('5')
       cy.get(':nth-child(2) > form > .btn').click()
       cy.get('#review').should("have.value", '')
       cy.get('#difficulty').should("have.value", '')
-      // Tests that form field clears after submiting a valid review.
 
     })
+  })
+
+  describe('Test that database was updated with the written review', function() {
+      it('Access the DB directly and searches the course document for the written review', function() {
+      cy.request('GET', 'http://it2810-39.idi.ntnu.no:3001/courses/IT2805').as('course')
+      cy.get('@course').should((response) => {
+          expect(response.body[0].reviews).to.contain(REVIEW_TEXT)
+      })
+      })
   })
 
